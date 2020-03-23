5ec70d255614b023b945eb34106d016eab7e6521
Paul <katzensteinerpaul@gmail.com>
2020-03-23 16:25:41 +0100

    Cookies integriert
    Cookie Warnung + Cookie der anzeigt welcher Knopf gedrückt wurde

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 init();
 
+$(document).ready(function() {
+    setTimeout(function() {
+        $("#cookieConsent").fadeIn(200);
+    }, 4000);
+    $("#closeCookieConsent, .cookieConsentOK").click(function() {
+        $("#cookieConsent").fadeOut(200);
+    });
+});
 
 
 function init() {
@@ -22,6 +30,6 @@ var LaenderTexte = [
 function changeLandText(land) {
     console.log(land);
 
-
+    document.cookie = "Land="+land+";";
     document.getElementById("text-next-grid").innerHTML = LaenderTexte[land - 1] + " nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est ollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et  Lorem ipsum dolor  laborum. Lorem ipsum dolor  id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut la";
 }
