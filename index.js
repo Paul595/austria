de92f68b6c1f6eb7b8904f0e0e6b6c7bcfd67bfe
Paul <katzensteinerpaul@gmail.com>
2020-03-30 14:57:38 +0200

    Sticky Header

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var clearvalue = null;
 
 $(document).ready(function() {
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    document.cookie = "LastVisit="+time+";";
-    
+    document.cookie = "LastVisit=" + time + ";";
+
     checkResize();
     setTimeout(function() {
         $("#cookieConsent").fadeIn(200);
@@ -19,32 +19,31 @@ $(document).ready(function() {
     clearvalue = $("#grid-div div").css("clear");
 });
 
-$(window).on('resize', function(){
-   checkResize();
+$(window).on('resize', function() {
+    checkResize();
 });
 
-function checkResize(){
-  
-    var win = $(this); 
+function checkResize() {
+
+    var win = $(this);
 
 
     //If - Braucht echt große Hilfe
-    if (win.width() <= 1171) {
+    /*if (win.width() <= 1171) {
         $(".breaker").css("background-color", "rgba(0, 0, 0, 0.075)");
-        
+
         $("#grid-div div").css("float", "left");
-       
+
         $("#grid-div div").css("clear", "none");
 
-    }else{ //else pfad entfernt nicht die #grid-div div formatierung egal was probiert
+    } else { //else pfad entfernt nicht die #grid-div div formatierung egal was probiert
         $(".breaker").css("background-color", "rgba(0, 0, 0, 0.075)");
+    }*/
 
-    }
-
-    if (win.width() <= 1277) { 
+    if (win.width() <= 1277) {
         $(".topnav a.active").css("background-color", "rgba(0, 0, 0, 0)");
         $(".topnav a.active").css("color", "red");
-    }else{
+    } else {
         $(".topnav a.active").css("background-color", "rgba(187, 19, 19, 0.562)");
         $(".topnav a.active").css("color", "white");
     }
@@ -56,6 +55,8 @@ function init() {
 }
 
 
+
+/* Changes text next to BundesländerWappen to LaenderTexte*/
 var LaenderTexte = [
     "Wien",
     "Niederösterreich",
@@ -71,6 +72,23 @@ var LaenderTexte = [
 function changeLandText(land) {
     console.log(land);
 
-    document.cookie = "Land="+land+";";
+    document.cookie = "Land=" + land + ";";
     document.getElementById("text-next-grid").innerHTML = LaenderTexte[land - 1];
+}
+
+/*Sticky header*/
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 740 || document.documentElement.scrollTop > 740) {
+        $(".topnav").css("background-color", "rgba(255, 255, 255, 0.881)");
+        $(".topnav a").css("color", "rgba(0, 0, 0)");
+        $(".topnav a.active").css("background-color", "rgba(187, 19, 19, 0.862)");
+
+    } else {
+        $(".topnav").css("background-color", "rgba(0, 0, 0, 0.281)");
+        $(".topnav a").css("color", "rgba(255, 255, 255)");
+        $(".topnav a.active").css("background-color", "rgba(187, 19, 19, 0.562)");
+
+    }
 }
